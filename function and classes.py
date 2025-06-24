224731eff967d2135b9739c9f67a098be555efbc
KayalvizhiVijayakumar <kayalvizhianu1800@gmail.com>
2025-06-24 09:51:11 +0530

    Add files via upload

---
+++ function and classes.py
@@ -0,0 +1,14 @@
+class Calculator ():
+    def add(self,a,b):
+     return a+b
+    def subtract(self,a,b):
+     return a-b
+    def multiply(self,a,b):
+      return a*b
+calculator =Calculator()  
+add = calculator.add (2,3)  
+print(add);
+subtract = calculator.subtract(5,4)
+print(subtract);
+multiply = calculator.multiply(2,3)
+print (multiply);
